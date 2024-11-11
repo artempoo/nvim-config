f7a656c1e68ab11e6744ae9560a90932f3646770
artempoo <77047966+artempoo@users.noreply.github.com>
2024-11-11 06:21:24 +0300

    маппинг

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -2,4 +2,4 @@ vim.g.mapleader = " "
 
 --NeoTree
 vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
-vim.keymap.set("n", "<leader>ef", ":Neotree focus<CR>")
+-- vim.keymap.set("n", "<leader>ef", ":Neotree focus<CR>")
