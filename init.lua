6293d12fdeb0b3bd80edf32e0111d81c48538396
artempoo <77047966+artempoo@users.noreply.github.com>
2025-04-12 12:19:46 +0300

    проверяю производительность

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
-require("config.lazy")
+-- require("config.lazy")
 require("config.option")
 require("config.mappings")
+
+vim.cmd("syntax off");
+-- vim.wo.foldmethod = "manual" -- отключает автоматическое фолдинг
+--       vim.wo.spell = false        -- отключает проверку орфографии
+--       vim.wo.wrap = false         -- отключает перенос строк
