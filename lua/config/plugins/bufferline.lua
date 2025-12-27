3c858fee628edd12c490338de4999223839992cf
artempoo <77047966+artempoo@users.noreply.github.com>
2025-12-27 04:49:39 +0300

    active buffers color red

--- lua/config/plugins/bufferline.lua
+++ lua/config/plugins/bufferline.lua
@@ -4,7 +4,7 @@ return {
 	dependencies = "nvim-tree/nvim-web-devicons",
 	config = function()
 		vim.opt.termguicolors = true
-		
+
 		-- Маппинги для управления буферами
 		vim.api.nvim_set_keymap("n", "<TAB>l", ":BufferLineCloseLeft<CR>", { noremap = true, silent = true })
 		vim.api.nvim_set_keymap("n", "<TAB>r", ":BufferLineCloseRight<CR>", { noremap = true, silent = true })
@@ -27,7 +27,7 @@ return {
 			highlights = {
 				close_button = {
 					fg = "#e0def4",
-					-- bg = "#eb6f92",
+					bg = "NONE",
 					bold = true,
 				},
 				close_button_visible = {
@@ -36,11 +36,41 @@ return {
 					bold = true,
 				},
 				close_button_selected = {
-					fg = "#eb6f92",
+					fg = "#fb4934", -- Красный крестик для активного буфера
 					bg = "NONE",
 					bold = true,
 				},
+				buffer_selected = {
+					fg = "#fb4934", -- Красный текст для активного буфера
+					bold = true,
+				},
 			},
+		})
+
+		-- Принудительно устанавливаем highlight для активного буфера
+		-- Используем автокоманду для переопределения после загрузки цветовой схемы
+		local function update_selected_highlight()
+			-- Красный крестик для активного буфера
+			vim.api.nvim_set_hl(0, "BufferLineCloseButtonSelected", {
+				fg = "#fb4934",
+				bg = "NONE",
+				bold = true,
+			})
+			-- Красный текст для активного буфера
+			vim.api.nvim_set_hl(0, "BufferLineBufferSelected", {
+				fg = "#fb4934",
+				bg = "NONE",
+				bold = true,
+			})
+		end
+
+		-- Применяем сразу и после загрузки цветовой схемы
+		vim.defer_fn(update_selected_highlight, 100)
+		vim.api.nvim_create_autocmd("ColorScheme", {
+			pattern = "*",
+			callback = function()
+				vim.defer_fn(update_selected_highlight, 100)
+			end,
 		})
 	end,
 }
