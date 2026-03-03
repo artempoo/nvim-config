8d69f62bdcd383518af6be0c9b24ec6d494fa7f2
artempoo <77047966+artempoo@users.noreply.github.com>
2026-03-04 01:08:16 +0300

    multi-cursor

--- lua/config/plugins/cmp.lua
+++ lua/config/plugins/cmp.lua
@@ -55,17 +55,38 @@ return {
 			}),
 		})
 
-		-- Специальная конфигурация для Java: без LSP-источника и автотрингера
+		-- Конфигурация для Java: включаем автодополнение
 		cmp.setup.filetype('java', {
-			-- Автотриггер выключаем (чтобы не дергать LSP на TextChangedI), вызываем вручную
-			completion = { autocomplete = false, keyword_length = 2 },
+			preselect = cmp.PreselectMode.Item,
+			completion = {
+				keyword_length = 1,
+			},
 			mapping = {
-				["<C-l>"] = cmp.mapping.complete(),          -- альтернатива Ctrl-Space
-				["<M-Space>"] = cmp.mapping.complete(),      -- ещё одна альтернатива
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.close(),
-				["<Tab>"] = cmp.mapping.select_next_item(),
-				["<S-Tab>"] = cmp.mapping.select_prev_item(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<Down>"] = cmp.mapping.select_next_item(),
+				["<Up>"] = cmp.mapping.select_prev_item(),
+				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
 			},
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
--- lua/config/plugins/jdtls.lua
+++ lua/config/plugins/jdtls.lua
@@ -1,7 +1,161 @@
 return {
 	"mfussenegger/nvim-jdtls",
+	dependencies = {
+		"nvim-lspconfig",
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	ft = { "java" },
+	config = function()
+		local jdtls = require("jdtls")
+		local cmp_lsp = require("cmp_nvim_lsp")
+
+		-- Настройка для Java файлов
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "java" },
+			callback = function()
+				-- Находим корень проекта
+				local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
+				local root_dir = require("jdtls.setup").find_root(root_markers)
+				if not root_dir then
+					return
+				end
+
+				-- Путь к jdtls (установлен через Mason)
+				local jdtls_path = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
+				
+				-- Находим launcher jar
+				local launcher_jar = vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
+				if launcher_jar == "" then
+					vim.notify("jdtls не найден. Установите через :Mason", vim.log.levels.WARN)
+					return
+				end
+
+				-- Определяем конфигурацию по ОС
+				local os_name = vim.loop.os_uname().sysname
+				local config_name = "config_linux"
+				if os_name == "Darwin" then
+					config_name = "config_mac"
+				elseif os_name == "Windows_NT" then
+					config_name = "config_win"
+				end
+				local config_dir = jdtls_path .. "/" .. config_name
+
+				-- Workspace директория
+				local project_name = vim.fn.fnamemodify(root_dir, ":p:h:t")
+				local workspace_dir = vim.fn.stdpath("data") .. "/jdtls-workspace/" .. project_name
+
+				-- Команда для запуска jdtls
+				local cmd = {
+					"java",
+					"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+					"-Dosgi.bundles.defaultStartLevel=4",
+					"-Declipse.product=org.eclipse.jdt.ls.core.product",
+					"-Dlog.protocol=true",
+					"-Dlog.level=ALL",
+					"-Xmx1g",
+					"--add-modules=ALL-SYSTEM",
+					"--add-opens",
+					"java.base/java.util=ALL-UNNAMED",
+					"--add-opens",
+					"java.base/java.lang=ALL-UNNAMED",
+				}
+
+				-- Поддержка Lombok (если установлен)
+				local lombok_path = jdtls_path .. "/lombok.jar"
+				if vim.fn.filereadable(lombok_path) == 1 then
+					table.insert(cmd, "-javaagent:" .. lombok_path)
+				end
+
+				-- Добавляем jar и параметры конфигурации
+				table.insert(cmd, "-jar")
+				table.insert(cmd, launcher_jar)
+				table.insert(cmd, "-configuration")
+				table.insert(cmd, config_dir)
+				table.insert(cmd, "-data")
+				table.insert(cmd, workspace_dir)
+
+				-- Capabilities для LSP (включаем поддержку snippets для автодополнения)
+				local capabilities = vim.tbl_deep_extend("force", {}, cmp_lsp.default_capabilities(), {
+					textDocument = {
+						completion = {
+							completionItem = {
+								snippetSupport = true,
+							},
+						},
+					},
+				})
+
+				-- Конфигурация jdtls
+				local config = {
+					cmd = cmd,
+					root_dir = root_dir,
+					settings = {
+						java = {
+							eclipse = {
+								downloadSources = true,
+							},
+							maven = {
+								downloadSources = true,
+							},
+							implementationsCodeLens = {
+								enabled = true,
+							},
+							referencesCodeLens = {
+								enabled = true,
+							},
+							references = {
+								includeDecompiledSources = true,
+							},
+							configuration = {
+								updateBuildConfiguration = "interactive",
+							},
+							completion = {
+								favoriteStaticMembers = {
+									"org.junit.Assert.*",
+									"org.junit.Assume.*",
+									"org.junit.jupiter.api.Assertions.*",
+									"org.junit.jupiter.api.Assumptions.*",
+									"org.junit.jupiter.api.DynamicContainer.*",
+									"org.junit.jupiter.api.DynamicTest.*",
+									"org.mockito.Mockito.*",
+									"org.mockito.ArgumentMatchers.*",
+									"org.mockito.Answers.*",
+								},
+							},
+							sources = {
+								organizeImports = {
+									starThreshold = 9999,
+									staticStarThreshold = 9999,
+								},
+							},
+							codeGeneration = {
+								toString = {
+									template = "${object.className} [${member.name()}=${member.value}, ${otherMembers}]",
+								},
+								useBlocks = true,
+							},
+						},
+					},
+					capabilities = capabilities,
+					init_options = {
+						bundles = {},
+					},
+					on_attach = function(client, bufnr)
+						-- Включаем форматирование по сохранению
+						if client.server_capabilities.documentFormattingProvider then
+							vim.api.nvim_create_autocmd("BufWritePre", {
+								buffer = bufnr,
+								callback = function()
+									vim.lsp.buf.format({ bufnr = bufnr })
+								end,
+							})
+						end
+					end,
+				}
+
+				-- Запускаем jdtls
+				jdtls.start_or_attach(config)
+			end,
+		})
+	end,
 }
-
-
-
-
